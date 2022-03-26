96dbc956032af2c22c50c26408d554b30522961a
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-26 11:38:38 -0300

    feat: create Job Details page

---
+++ src/pages/jobs/[id].jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import Layout from "../../components/layout/Layout";
+
+export default function JobDetailsPage({ job, candidates }) {
+  return (
+    <Layout>
+      <h1>Job Details</h1>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const res = await axios.get(`${process.env.API_URL}/api/jobs/${params.id}/`);
+  const job = res.data.job;
+  const candidates = res.data.candidates;
+  return {
+    props: {
+      job,
+      candidates
+    }
+  }
+}
