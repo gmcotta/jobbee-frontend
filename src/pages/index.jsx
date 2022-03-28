a48848977932b98a4169c726bc2febcf70fc755d
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-28 14:15:03 -0300

    feat: get jobs with query params

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,8 +11,13 @@ export default function Index({ data }) {
   )
 }
 
-export async function getServerSideProps() {
-  const res = await axios.get(`${process.env.API_URL}/api/jobs/`);
+export async function getServerSideProps({ query }) {
+  const keyword = query.keyword || '';
+  const location = query.location || '';
+  const queryString = new URLSearchParams();
+  keyword && queryString.append('keyword', keyword);
+  location && queryString.append('location', location);
+  const res = await axios.get(`${process.env.API_URL}/api/jobs?${queryString.toString()}`);
   const data = res.data;
 
   return {
