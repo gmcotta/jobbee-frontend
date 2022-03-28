0a2daa1cfebe26f47c7c76bad0fcadb150b59d44
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-28 17:57:24 -0300

    feat: create Login component

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import { useAuth } from '../../hooks/useAuth';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const { loading, error, isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    if (error) console.log(error);
+    if (isAuthenticated && !loading) router.push('/');
+  }, [isAuthenticated, error, loading, router]);
+
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+    console.log({ email, password });
+  }
+
+  return (
+    <div className="modalMask">
+      <div className="modalWrapper">
+        <div className="left">
+          <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <Image 
+              priority="true" 
+              layout="fill" 
+              src="/images/login.svg" 
+              alt="login" 
+            />
+          </div>
+        </div>
+        <div className="right">
+          <div className="rightContentWrapper">
+            <div className="headerWrapper">
+              <h2> LOGIN</h2>
+            </div>
+            <form className="form" onSubmit={submitHandler}>
+              <div className="inputWrapper">
+                <div className="inputBox">
+                  <i aria-hidden className="fas fa-envelope"></i>
+                  <input 
+                    type="email" 
+                    placeholder="Enter Your Email" 
+                    required 
+                    value={email}
+                    onChange={(evt) => setEmail(evt.target.value)}
+                    title="Your email is invalid"
+                    pattern='\S+@\S+\.\S+'
+                  />
+                </div>
+                <div className="inputBox">
+                  <i aria-hidden className="fas fa-key"></i>
+                  <input
+                    type="password"
+                    placeholder="Enter Your Password"
+                    required
+                    value={password}
+                    onChange={(evt) => setPassword(evt.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="loginButtonWrapper">
+                <button type="submit" className="loginButton">
+                  {loading ? 'Authenticating' : 'Login'}
+                </button>
+              </div>
+              <p style={{ textDecoration: "none" }} className="signup">
+                New to Jobbee? <a href="/register">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
