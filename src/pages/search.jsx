df434df69919fa3d9a1a199987e09d9a0c879130
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-28 14:14:10 -0300

    feat: create Search page

---
+++ src/pages/search.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout/Layout";
+import Search from "../components/layout/Search";
+
+export default function SearchPage() {
+  return (
+    <Layout title="Jobbee - Search your jobs">
+      <Search />
+    </Layout>
+  );
+}
