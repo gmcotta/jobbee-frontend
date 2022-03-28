d02d5d87c28f681a3e94193a9d8002eb52afcafb
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-28 12:11:26 -0300

    feat: apply NotFound in 404 and JobDetails pages

--- src/pages/jobs/[id].jsx
+++ src/pages/jobs/[id].jsx
@@ -1,23 +1,38 @@
 import axios from "axios";
 import JobDetails from "../../components/job/JobDetails";
 import Layout from "../../components/layout/Layout";
+import NotFound from "../../components/layout/NotFound";
 
-export default function JobDetailsPage({ job, candidates }) {
+export default function JobDetailsPage({ job, candidates, error }) {
+  console.log(error);
+  if (error === 'Not found.') {
+    return (
+      <Layout title="Jobbee - Page not found"><NotFound /></Layout>
+    );
+  }
   return (
-    <Layout>
+    <Layout title={`Jobbee - ${job.title}`}>
       <JobDetails job={job} candidates={candidates} />
     </Layout>
   )
 }
 
 export async function getServerSideProps({ params }) {
-  const res = await axios.get(`${process.env.API_URL}/api/jobs/${params.id}/`);
-  const job = res.data.job;
-  const candidates = res.data.candidates;
-  return {
-    props: {
-      job,
-      candidates
+  try {
+    const res = await axios.get(`${process.env.API_URL}/api/jobs/${params.id}/`);
+    const job = res.data.job;
+    const candidates = res.data.candidates;
+    return {
+      props: {
+        job,
+        candidates
+      }
+    }
+  } catch (err) {
+    return {
+      props: {
+        error: err.response.data.detail
+      }
     }
   }
 }
