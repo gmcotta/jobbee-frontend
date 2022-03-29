2fb5852a617ce29f537f9d5f8ce49b7fd53464ab
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-29 15:38:22 -0300

    feat: create JobContext and hook

---
+++ src/contexts/JobContext.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, createContext } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+export const JobContext = createContext();
+
+export const JobProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [updated, setUpdated] = useState(false);
+  const [applied, setApplied] = useState(false);
+
+  const clearErrors = () => {
+    setError(false);
+  }
+
+  const applyToJob = async ({ id, accessToken }) => {
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/jobs/${id}/apply/`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        }
+      );
+      if (res.data.applied) {
+        setLoading(false);
+        setApplied(true);
+      }
+    } catch (err) {
+      setLoading(false);
+      setError(err.response && err.response.data.detail || err.response.data.error);
+    }
+  }
+
+  return (
+    <JobContext.Provider
+      value={{ 
+        loading,
+        error,
+        updated,
+        applied,
+        setUpdated,
+        clearErrors,
+        applyToJob
+      }}
+    >
+      {children}
+    </JobContext.Provider>
+  );
+}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,38 +1,41 @@
 import Script from 'next/script';
 import { AuthProvider } from '../contexts/AuthContext';
+import { JobProvider } from '../contexts/JobContext';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      <JobProvider>
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous"
+        />
+
+        <Script
+          strategy="beforeInteractive"
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        ></Script>
+
+        <Script
+          src="https://kit.fontawesome.com/9edb65c86a.js"
           crossOrigin="anonymous"
-      />
+        ></Script>
 
-      <Script
-        strategy="beforeInteractive"
-        src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      ></Script>
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        ></Script>
 
-      <Script
-        src="https://kit.fontawesome.com/9edb65c86a.js"
-        crossOrigin="anonymous"
-      ></Script>
-
-      <Script
-        strategy="beforeInteractive"
-        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-      ></Script>
-
-      <Script
-        strategy="beforeInteractive"
-        src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-      ></Script>
-      <Component {...pageProps} />
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        ></Script>
+        <Component {...pageProps} />
+      </JobProvider>
     </AuthProvider>
   )
 }
