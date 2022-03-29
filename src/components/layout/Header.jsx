d59508b0813a1022b748c0f82c80c380063d4870
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-29 11:17:27 -0300

    feat: display user in Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useAuth } from '../../hooks/useAuth';
 
 const Header = () => {
+  const { loading, user } = useAuth();
   return (
     <div className="navWrapper">
       <div className="navContainer">
@@ -21,12 +23,54 @@ const Header = () => {
               <span>Post A Job</span>
             </button>
           </Link>
-
-          <Link passHref href="/login">
-            <button className="loginButtonHeader">
-              <span>Login</span>
-            </button>
-          </Link>
+          {user ? (
+            <div className="dropdown-ml-3">
+              <a 
+                id="dropdown-menu" 
+                data-toggle="dropdown" 
+                aria-haspopup="true" 
+                aria-expanded="false" 
+                className="btn dropdown-toggle mr-4"
+              >
+                <span>Hi, {user.first_name}</span>
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdown-menu">
+                <Link passHref href="/employer/jobs">
+                  <a className="dropdown-item">
+                    My Jobs
+                  </a>
+                </Link>
+                <Link passHref href="/me/applied">
+                  <a className="dropdown-item">
+                    Jobs Applied
+                  </a>
+                </Link>
+                <Link passHref href="/me">
+                  <a className="dropdown-item">
+                    Profile
+                  </a>
+                </Link>
+                <Link passHref href="/upload/resume">
+                  <a className="dropdown-item">
+                    Upload Resume
+                  </a>
+                </Link>
+                <Link passHref href="/">
+                  <a className="dropdown-item text-danger">
+                    Logout
+                  </a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link passHref href="/login">
+                <button className="loginButtonHeader">
+                  <span>Login</span>
+                </button>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </div>
