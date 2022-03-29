65d120022c9f6e2a4104f1306b85428abf7729d2
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-29 10:31:05 -0300

    feat: create get user function and api route

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 
@@ -11,17 +11,39 @@ export const AuthProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const router = useRouter();
 
+  useEffect(() => {
+    if(!user) loadUser();
+  }, [user]);
+
   const login = async ({ username, password }) => {
     try {
       setLoading(true);
       const res = await axios.post('/api/auth/login', { username, password });
       if (res.data.success) {
         setIsAuthenticated(true);
+        loadUser();
         setLoading(false);
         router.push('/');
       }
     } catch (err) {
       setLoading(false);
+      setError(err.response && err.response.data.detail || err.response.data.error);
+    }
+  }
+
+  const loadUser = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get('/api/auth/user');
+      if (res.data.user) {
+        setIsAuthenticated(true);
+        setLoading(false);
+        setUser(res.data.user);
+      }
+    } catch (err) {
+      setLoading(false);
+      setIsAuthenticated(false);
+      setUser(null);
       setError(err.response && err.response.data.detail || err.response.data.error);
     }
   }
---
+++ src/pages/api/auth/user.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import cookie from 'cookie';
+
+const User = async (req, res) => {
+  if (req.method === 'GET') {
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const access = cookies.access || false;
+    console.log(access);
+    if (!access) {
+      return res
+        .status(401)
+        .json({
+          error: 'Login first to load user'
+        }
+      );
+    }
+    try {
+      const response = await axios.get(
+        `${process.env.API_URL}/api/me/`, 
+        {
+          headers: {
+            'Authorization': `Bearer ${access}`
+          }
+        }
+      );
+      if (response.data) {
+        return res
+          .status(200)
+          .json({
+            user: response.data
+          }
+        );
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: 'Something went wrong while retrieving user'
+      });
+    }
+  }
+}
+
+export default User;
